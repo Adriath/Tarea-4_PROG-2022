850439b0e21905b66b54ea92d1a52b99173278b1
Adriath <adrianarjonabravo@gmail.com>
2022-12-04 18:18:53 +0100

    Creación del proyecto y comienzo del supuesto 1. También he agregado un paquete con una clase psra excepciones y otra con utilides.

---
+++ src/recursos/Excepciones.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursos;
+
+/**
+ * Clase dedicada a las excepciones personalizadas para utilizar en los 
+ * distiontos supuestos.
+ * 
+ * @author Adrián Arjona
+ * @version 1.0 Diciembre 2022
+ */
+public class Excepciones {
+    
+     /* Francisco Adrián Arjona Bravo
+        UNIDAD 4: uso de estructuras de control.
+    */
+    
+}
---
+++ src/supuesto1/Notas_Adrian.java
@@ -0,0 +1,26 @@
+
+package supuesto1;
+
+/**
+ * SUPUESTO 1: IF ELSE Y EXCEPCIONES
+ * Realizar una clase llamada Notas_Nombre (Nombre es vuestro nombre), que 
+ * llevará un método main(), en elque se implementará un algoritmo que realizará 
+ * lo siguiente utilizando if anidados (if else):
+ * 
+ * @author Adrián Arjona
+ * @version 1.0 Diciembre 2022
+ */
+public class Notas_Adrian {
+   
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 4: uso de estructuras de control.
+    */
+    
+    static float nota ;
+    static String nombre ;
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Bievenido/a al calificador.\n");
+    }
+}

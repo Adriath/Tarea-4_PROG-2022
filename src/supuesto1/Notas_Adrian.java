a9051aacdf50ca7fbce8d5a7814a5d0c1d906e7e
Adriath <adrianarjonabravo@gmail.com>
2022-12-05 13:45:45 +0100

    He realiado un par de métodos y constructores. Todo está preparado para implementar en el main.

--- src/supuesto1/Notas_Adrian.java
+++ src/supuesto1/Notas_Adrian.java
@@ -2,6 +2,7 @@
 package supuesto1;
 
 import java.lang.ref.SoftReference;
+import recursos.excepciones.ExcepcionRangoNotas;
 import recursos.utilidades.Utilidades;
 
 /**
@@ -21,19 +22,84 @@ public class Notas_Adrian{
     
     // DECLARACIÓN DE VARIABLES
     
-    static float nota ;
-    static String nombre ;
+    private float nota ;
+    private String nombre ;
     
     public enum Notas {Suspenso, Aprobado, Bien, Notable, Sobresaliente, Matrícula} ;
     
-    public static String compruebaNota (int n){
-        
-        String nota = "" ;
-        Notas num ;
-        
-        if (n > 5) {
-            nota = Notas.Aprobado.name() ;
+    // CONSTRUCTOR
+    
+    public Notas_Adrian(String nombre, float nota){
+     
+        this.nota = nota ;
+        this.nombre = nombre ;
+    }
+    
+    // MÉTODOS
+    
+        // GETTERS & SETTERS
+    
+            // NOTA
+    
+        public float getNota() {
+            return nota;
         }
+
+        public void setNota(float nota) {
+            this.nota = nota;
+        }
+
+            // NOMBRE
+    
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+    
+    public void compruebaRangoNota(float nota) throws ExcepcionRangoNotas{
+        
+        if (nota >= 0 & nota <= 10) // Si la nota es mayor o igual que cero y menor o igual que 10...
+        {
+            this.nota = nota ; // ...es válido y se guardará.
+        }
+        else // Si no se cumple condición...
+        {
+            throw new ExcepcionRangoNotas("La nota debe estar comprendida entre 0 y 10.") ; // ...saltará la excepción.
+        }
+    }
+    
+    public String equivalenciaNota(float n){
+        
+        String nota = "";
+        
+        if (n < 5) // Si la nota es menor a 5...
+        {
+            nota = Notas.Suspenso.name() ; // ...suspenso.
+        }
+        else if (n >= 5 && n < 6 ) // Si la nota es mayor o igual a 5 y menor que 6...
+        {
+            nota = Notas.Aprobado.name() ; // ...aprobado.
+        }
+        else if (n >= 6 && n < 7 ) // Si la nota es mayor o igual a 6 y menor que 7...
+        {
+            nota = Notas.Bien.name() ; // ...bien.
+        }
+        else if (n >= 7 && n < 9 ) // Si la nota es mayor o igual a 7 y menor que 9...
+        {
+            nota = Notas.Notable.name() ; // ...notable
+        }
+        else if (n >= 9 && n < 10 ) // Si la nota es mayor o igual a 9 y menor que 10...
+        {
+            nota = Notas.Sobresaliente.name() ; // ...sobresaliente.
+        }
+        else if (n == 10 ) // Si la nota es igual a 10...
+        {
+            nota = Notas.Matrícula.name() ; // ...matrícula.
+        }
+        
         return nota ;
     }
     
@@ -41,7 +107,8 @@ public class Notas_Adrian{
         
         System.out.println("Bievenido/a al calificador.\n");
         
-        System.out.println("El resultado es: " + compruebaNota(6));
+        
     }
+    
     
 }

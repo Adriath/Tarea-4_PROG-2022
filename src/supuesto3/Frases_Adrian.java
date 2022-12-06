b1bdb95066b819c16f0083ffb4eba13ec3aa94fb
Adriath <adrianarjonabravo@gmail.com>
2022-12-06 15:55:23 +0100

    Estoy agregando los refranes.

---
+++ src/supuesto3/Frases_Adrian.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package supuesto3;
+
+/**
+ * Realiza un programa llamado Frases_Nombre, que muestre un menú de opciones 
+ * que permitan hacer lo siguiente:
+ *  1. Mostrar frase motivacional.
+ *  2. Mostrar frase pesimista.
+ *  3. Contar un chiste.
+ *  4. Decir un refrán.
+ *  5. Salir.
+ * 
+ * @author Adrián Arjona
+ * @version 1.0 Diciembre 2022
+ */
+public class Frases_Adrian {
+    
+     /* Francisco Adrián Arjona Bravo
+        UNIDAD 4: uso de estructuras de control.
+    */
+    
+    // --------- DECLARACIÓN DE VARIABLES --------------
+    
+        //Frases motivacionales o(???)?
+    
+        private String fraseMotivacional1 = "\nLa informática no es solo una "
+                + "herramienta, es una forma de pensar y una oportunidad para "
+                + "aplicar la lógica y la creatividad en la resolución de "
+                + "problemas.\n" ;
+        private String fraseMotivacional2 = "\nLa informática es el futuro, así "
+                + "que abraza la tecnología y conviértete en un maestro de la "
+                + "informática para alcanzar tus sueños y lograr el éxito.\n" ;
+        private String fraseMotivacional3 = "\nEl éxito no es una meta, es un "
+                + "viaje en el que debes mantenerte en movimiento y perseverar "
+                + "en tus metas y sueños.\n" ;
+        
+        // Frases pesimistas ?|?|_
+        
+        private String frasePesimista1 = "\nNunca lograré lo que quiero en la vida.\n" ;
+        private String frasePesimista2 = "\nTodo lo que empieza bien, termina mal\n" ;
+        private String frasePesimista3 = "\nSi algo malo puede pasar, pasará.\n" ;
+        
+        // Chistes (???)
+        
+        private String chiste1 = "\nJaimito en clase:\n "
+                + "\t-\"Hoy vamos a dar los poemas. A ver, Juanito, hazme un pareado\".\n "
+                + "\t-\"Fui a recoger almejas y me llegaba el agua por las orejas\".\n"
+                + "\t-\"Muy bien, Juanito. Ahora tú, Jaimito\".\n"
+                + "\t-\"Fui a recoger mejillones y me llegaba el agua por las rodillas\".\n"
+                + "\t-\"Jaimito, eso no rima\".\n"
+                + "\t-\"Pues espérate a que suba la marea\"." ;
+                
+        // Refranes (o???)o?
+    
+        
+}

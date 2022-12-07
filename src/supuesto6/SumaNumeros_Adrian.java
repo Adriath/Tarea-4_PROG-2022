bc656b74568f63a6204ef5d0871288326ddaebcc
Adriath <adrianarjonabravo@gmail.com>
2022-12-07 20:41:14 +0100

    Creación del supuesto 6.

---
+++ src/supuesto6/SumaNumeros_Adrian.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package supuesto6;
+
+/**
+ * SUPUESTO 6
+ * 
+ * Hay que realizar un programa llamado SumaNumeros_Nombre, en el que dada una 
+ * secuencia de números leídos por teclado, que acabe con -1, el programa tras 
+ * leer dichos números muestre la suma de todos ellos (a excepción del número -1, 
+ * el cual es escrito solo para finalizar el programa). Puedes utilizar la 
+ * instrucción iterativa que veas más oportuna para realizar el ejercicio.
+ * 
+ * @author Adrián Arjona
+ * @version 1.0 Diciembre 2022
+ */
+public class SumaNumeros_Adrian {
+    
+    
+    /* Francisco Adrián Arjona Bravo
+        UNIDAD 4: uso de estructuras de control.
+    */
+    
+    
+}
